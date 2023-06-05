dfe89cd5b944a5b24266e8cdca14ac15066365d7
WFH Brian <brian@plexm.com>
2023-06-05 12:50:14 -0400

    up

--- esbuild.js
+++ esbuild.js
@@ -1,4 +1,10 @@
 const esbuild = require('esbuild');
+// update manifest.json to include version number from package.json
+const fs = require('fs');
+const packageJson = JSON.parse(fs.readFileSync('./package.json'));
+const manifestJson = JSON.parse(fs.readFileSync('./manifest.json'));
+manifestJson.version = packageJson.version;
+fs.writeFileSync('./manifest.json', JSON.stringify(manifestJson, null, 2));
 
 esbuild.build({
   entryPoints: ['src/index.js'],
