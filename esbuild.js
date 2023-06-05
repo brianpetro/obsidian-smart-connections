c791966fee31f5d16140a752924bbdf55e4e42b8
WFH Brian <brian@plexm.com>
2023-06-05 14:25:45 -0400

    up

--- esbuild.js
+++ esbuild.js
@@ -1,17 +1,18 @@
 const esbuild = require('esbuild');
-// update manifest.json to include version number from package.json
 const fs = require('fs');
 const packageJson = JSON.parse(fs.readFileSync('./package.json'));
-const manifestJson = JSON.parse(fs.readFileSync('./manifest.json'));
+const manifestJson = JSON.parse(fs.readFileSync('./src/manifest.json'));
 manifestJson.version = packageJson.version;
 fs.writeFileSync('./dist/manifest.json', JSON.stringify(manifestJson, null, 2));
+
+// Copy styles.css to dist folder
+fs.copyFileSync('./src/styles.css', './dist/styles.css');
+
 
 esbuild.build({
   entryPoints: ['src/index.js'],
   outfile: 'dist/main.js',
   bundle: true,
-  copyFiles: {
-    'src/styles.css': 'dist/styles.css',
-  },
+  write: true,
   external: ['obsidian', 'crypto'],
 }).catch(() => process.exit(1));
