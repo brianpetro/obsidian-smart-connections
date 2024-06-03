661ece41708f30ed96aa5976518aaecfa14623f0
WFH Brian <brian@plexm.com>
2024-06-03 16:38:09 -0400

    better scroll to block position

--- src/open_note.js
+++ src/open_note.js
@@ -1,35 +1,11 @@
 async function open_note(plugin, target_path, event=null) {
+  const env = plugin.env;
   let targetFile;
-  let heading;
-  // if has # and is last character, remove it
-  if (target_path[target_path.length - 1] === "#") target_path = target_path.slice(0, -1);
-  if (target_path.indexOf("#") > -1) {
+  let block;
+  if (target_path.endsWith("#")) target_path = target_path.slice(0, -1);
+  if (target_path.includes("#")) {
     targetFile = plugin.app.metadataCache.getFirstLinkpathDest(target_path.split("#")[0], ""); // remove after # from link
-    // console.log(targetFile);
-    const target_file_cache = plugin.app.metadataCache.getFileCache(targetFile);
-    // console.log(target_file_cache);
-    let heading_text = target_path.split("#").pop(); // get heading
-    // if heading text contains a curly brace, get the number inside the curly braces as occurence
-    let occurence = 0;
-    if (heading_text.indexOf("{") > -1) {
-      // get occurence
-      occurence = parseInt(heading_text.split("{")[1].split("}")[0]);
-      // remove occurence from heading text
-      heading_text = heading_text.split("{")[0];
-    }
-    const headings = target_file_cache.headings; // get headings from file cache
-    // get headings with the same depth and text as the link
-    for(let i = 0; i < headings.length; i++) {
-      if (headings[i].heading === heading_text) {
-        // if occurence is 0, set heading and break
-        if(occurence === 0) {
-          heading = headings[i];
-          break;
-        }
-        occurence--; // decrement occurence
-      }
-    }
-    // console.log(heading);
+    block = env.smart_blocks.get(target_path);
   } else {
     targetFile = plugin.app.metadataCache.getFirstLinkpathDest(target_path, "");
   }
@@ -41,9 +17,9 @@ async function open_note(plugin, target_path, event=null) {
     leaf = plugin.app.workspace.getMostRecentLeaf(); // get most recent leaf
   }
   await leaf.openFile(targetFile);
-  if (heading) {
+  if(block?.line_start) {
     let { editor } = leaf.view;
-    const pos = { line: heading.position.start.line, ch: 0 };
+    const pos = { line: block.line_start, ch: 0 };
     editor.setCursor(pos);
     editor.scrollIntoView({ to: pos, from: pos }, true);
   }
