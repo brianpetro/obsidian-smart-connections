db58a5961927b21c5762cb377dc627a3d6f3770e
WFH Brian <brian@plexm.com>
2024-11-28 07:40:35 -0500

    bump early release version

--- src/components/main_settings.js
+++ src/components/main_settings.js
@@ -79,9 +79,9 @@ function render_supporters_section(scope) {
   ;
   const become_supporter_html = scope.EARLY_ACCESS ? '' : `<div class="setting-component"
       data-name="Upgrade to Early Access Version (v2.2)"
-      data-description="Upgrade to v2.2 (Early Access) to access new features and improvements."
+      data-description="Upgrade to v2.3 (Early Access) to access new features and improvements."
       data-type="button"
-      data-btn-text="Upgrade to v2.2"
+      data-btn-text="Upgrade to v2.3"
       data-callback="update_early_access"
     ></div>
     <div class="setting-component"
