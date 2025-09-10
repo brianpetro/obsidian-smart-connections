37071ce902d7c206dd7430e2738977d23c18316d
Brian Joseph Petro <brian@smartconnections.app>
2025-09-10 11:17:49 -0400

    feat: emit event when a new plugin version is available

--- src/index.js
+++ src/index.js
@@ -217,6 +217,7 @@ export default class SmartConnectionsPlugin extends Plugin {
       });
       const latest_release = response.tag_name;
       if(latest_release !== this.manifest.version) {
+        this.env?.events?.emit('plugin:new_version_available', { version: latest_release });
         this.notices?.show('new_version_available', {version: latest_release});
         this.update_available = true;
       }
