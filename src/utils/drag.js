285b30abeb8878637c400c29799b339bcbe71c68
Brian Joseph Petro <brian@smartconnections.app>
2025-10-17 09:25:24 -0400

    fix: replace deprecated parse_key_to_link function with parse_item_key_to_wikilink in handle_drag_result

--- src/utils/drag.js
+++ src/utils/drag.js
@@ -1,16 +1,8 @@
-export function parse_key_to_link(key){
-  if(!key) return '';
-  const [file_path, ...parts] = key.split('#');
-  const file_name = file_path.split('/').pop().replace(/\.md$/, '');
-  if(!parts.length) return `[[${file_name}]]`;
-  const heading = parts.filter(part => !part.startsWith('{')).pop();
-  if(!heading) return `[[${file_name}]]`;
-  return `[[${file_name}#${heading}]]`;
-}
+import { parse_item_key_to_wikilink } from "obsidian-smart-env/utils/parse_item_key_to_wikilink";
 
 export function handle_drag_result(app, event, key){
   const drag_manager = app.dragManager;
-  const link_text = parse_key_to_link(key);
+  const link_text = parse_item_key_to_wikilink(key);
   const drag_data = drag_manager.dragLink(event, link_text);
   drag_manager.onDragStart(event, drag_data);
 }
