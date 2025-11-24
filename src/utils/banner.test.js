bc837c6d0f5605dc1348212c3aa47e9a4ff6be6c
Brian Joseph Petro <brian@smartconnections.app>
2025-11-24 15:13:28 -0500

    refactor: remove obsolete banner test file

--- src/utils/banner.test.js
+++
@@ -1,8 +0,0 @@
-import test from 'ava';
-import { create_banner } from './banner.js';
-
-test('create_banner includes name, version, author and year', t => {
-  const banner = create_banner({ name: 'pkg', version: '1.0.0', author: 'Author' });
-  const year = new Date().getFullYear();
-  t.is(banner, `/*! pkg v1.0.0 | (c) ${year} Author */`);
-});
